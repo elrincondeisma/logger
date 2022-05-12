15267bd30ae0dd44365e20cccde113b25fca04e7
Ismael Catalá Gil <ismael.catala@smileandlearn.com>
2022-05-12 22:48:48 +0200

    bbdd

--- src/Logger.php
+++ src/Logger.php
@@ -1,12 +1,25 @@
 <?php
 
 namespace Icatala\Logger;
+use Icatala\Logger\Models\Log;
 
 class Logger
 {
     // Build your next great package.
-    public function create($name){
-        return 'Hi ' . $name . '! How are you doing today?';
+    public function create($model,$msg){
+        try {
+            //code...
+            $log = new Log;
+            $log->model = $model;
+            $log->message = $msg;
+            $log->save();
+            return $data = array('status' => 'ok','log'=>$log );
+        } catch (\Exception $th) {
+            //throw $th;
+            return $data = array('status' => 'ko','error'=>$th );
+
+
+        }
 
     }
 }
